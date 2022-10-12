c22660ee9501189a07b442c0c3951004c815d3b8
jong gyu <jgg5821@naver.com>
2022-10-12 18:03:13 +0900

    asdfsd

---
+++ src/Word.java
@@ -0,0 +1,11 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Word {
+	private String elglish; // 영어 단어
+	private String korean; // 한글 단어
+
+}
